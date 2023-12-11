3c807c8c191f48758c6d1f735f73318e2dead79e
Luca Zanchetta <zanchetta.1848878@studenti.uniroma1.it>
2023-12-11 14:31:13 +0100

    APIs skeleton

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-import psycopg2
+import mysql.connector
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
@@ -17,11 +17,95 @@ def before_request():
         }
         return ("", 200, headers)
 
-
-
-@app.route("/")
+@app.route('/')
 def fetch():
     return jsonify({"message":"The server is working! :)", "status":200})
+
+
+############################ ACCOUNT MANAGEMENT APIs #####################################
+@app.route('/register', methods=['POST'])
+def create_account():
+    return jsonify({"message":"WORK IN PROGRESS...", "status":202})
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    return jsonify({"message":"WORK IN PROGRESS...", "status":202})
+
+
+@app.route('/updateAccount', methods=['POST'])
+def update_account():
+    return jsonify({"message":"WORK IN PROGRESS...", "status":202})
+
+
+@app.route('/deleteAccount', methods=['POST'])
+def delete_account():
+    return jsonify({"message":"WORK IN PROGRESS...", "status":202})
+
+
+########################## PICTURES & POSTS MANAGEMENT APIs ##############################
+@app.route('/createPost', methods=['POST'])
+def create_post():
+    return jsonify({"message":"WORK IN PROGRESS...", "status":202})
+
+
+@app.route('/deletePost', methods=['POST'])
+def delete_post():
+    return jsonify({"message":"WORK IN PROGRESS...", "status":202})
+
+
+# Maybe there should be another api for the shirt try-on, but I don't know
+
+############################## FRIENDS MANAGEMENT APIs ################################
+@app.route('/getUserByUsername', methods=['GET'])
+def get_user_by_username():
+    return jsonify({"message":"WORK IN PROGRESS...", "status":202})
+
+
+@app.route('/getNotifications', methods=['GET'])
+def get_notifications():
+    return jsonify({"message":"WORK IN PROGRESS...", "status":202})
+
+
+@app.route('/sendFriendshipRequest', methods=['POST'])
+def send_friendship_request():
+    return jsonify({"message":"WORK IN PROGRESS...", "status":202})
+
+
+@app.route('/acceptFriendshipRequest', methods=['POST'])
+def accept_friendship_request():
+    return jsonify({"message":"WORK IN PROGRESS...", "status":202})
+
+
+@app.route('/refuseFriendshipRequest', methods=['POST'])
+def refuse_friendship_request():
+    return jsonify({"message":"WORK IN PROGRESS...", "status":202})
+
+
+@app.route('/removeFriend', methods=['POST'])
+def remove_friend():
+    return jsonify({"message":"WORK IN PROGRESS...", "status":202})
+
+
+@app.route('/getFriendsPosts', methods=['GET'])
+def get_friends_posts():
+    return jsonify({"message":"WORK IN PROGRESS...", "status":202})
+
+
+@app.route('/getPostsByUsername', methods=['GET'])
+def get_posts_by_username():
+    return jsonify({"message":"WORK IN PROGRESS...", "status":202})
+
+
+########################### INTERACTIONS MANAGEMENT APIs #################################
+@app.route('/likePost', methods=['POST'])
+def like_post():
+    return jsonify({"message":"WORK IN PROGRESS...", "status":202})
+
+
+@app.route('/getLikesByPost', methods=['GET'])
+def get_likes_by_post():
+    return jsonify({"message":"WORK IN PROGRESS...", "status":202})
 
 
 

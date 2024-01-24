4879e227af56e0c0e8ca0f37e81a91b5e35d0192
camillaiorio <camy.iorio@gmail.com>
2024-01-24 10:50:40 +0100

    NavBar fatta

--- frontend/app/src/main/java/com/example/styleup/FeedActivity.kt
+++ frontend/app/src/main/java/com/example/styleup/FeedActivity.kt
@@ -3,21 +3,71 @@ package com.example.styleup
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class FeedActivity: AppCompatActivity() {
+
+    private lateinit var icon1: ImageView
+    private lateinit var icon2: ImageView
+    private lateinit var icon3: ImageView
+    private lateinit var icon4: ImageView
+
+    private lateinit var toolbar: Toolbar
+
+    private lateinit var mainFragmentContainer: FrameLayout
+    private lateinit var fragmentManager: FragmentManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed)
 
-        val notificationIcon: ImageView = findViewById(R.id.notificationIcon)
 
-        // Gestisci il clic sull'icona delle notifiche, ad esempio mostra un'attività delle notifiche
+        val notificationIcon: ImageView = findViewById(R.id.notificationIcon)
         notificationIcon.setOnClickListener {
-            // Aggiungi qui la logica per mostrare le notifiche
             showConfirmationDialog()
         }
+
+        mainFragmentContainer = findViewById(R.id.mainFragmentContainers)
+        fragmentManager = supportFragmentManager
+
+        val shirtsFragment = ShirtsFragment()
+        setMainFragment(shirtsFragment)
+
+        icon1 = findViewById(R.id.icon1)
+        icon2 = findViewById(R.id.icon2)
+        icon3 = findViewById(R.id.icon3)
+        icon4 = findViewById(R.id.icon4)
+
+        icon1.setOnClickListener {
+            val friendsFragment = FriendsFragment()
+            setMainFragment(friendsFragment)
+        }
+
+        icon2.setOnClickListener {
+            setMainFragment(ShirtsFragment())
+        }
+
+        icon3.setOnClickListener {
+            val profileFragment = ProfileFragment()
+            setMainFragment(profileFragment)
+        }
+
+        icon4.setOnClickListener {
+            val mapFragment = MapFragment()
+            setMainFragment(mapFragment)
+        }
+
+    }
+
+    private fun setMainFragment(fragment: Fragment) {
+        val transaction: FragmentTransaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.mainFragmentContainers, fragment)
+        transaction.commit()
     }
 
     private fun showConfirmationDialog() {
